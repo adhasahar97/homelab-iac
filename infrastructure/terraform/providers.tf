b1b23f3a81765a6dbe92252bf62fd4007369162b
AdhaSahar <adhasahar97@gmail.com>
2024-07-30 00:05:09 +0800

    Test cloudflare tunnel using terraform

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -7,3 +7,16 @@ provider "helm" {
     config_path = "~/.kube/config"
   }
 }
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
